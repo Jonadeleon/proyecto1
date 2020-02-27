210192c1f462c270bbc4d79d04f382ff5521673f
jonathan.deleon <jondeleonsola@gmail.com>
2020-02-27 12:41:02 +0100

    Paginas principales - falta conectar a base de datos

---
+++ index.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, flash, redirect
+# traemos dependecia de pymongo para la conexion a la base de datos
+from pymongo import MongoClient
+
+app = Flask(__name__)
+# ponemos la conexion y la base de datos
+client = MongoClient('localhost', 27017)
+# db =
+
+
+@app.route('/')
+def inicio():
+    return render_template('index.html')
+
+@app.route('/clases')
+def clases():
+    return render_template('clases.html')
+
+@app.route('/razas')
+def razas():
+    return render_template('razas.html')
+
+@app.route('/armaduras')
+def armaduras():
+    return render_template('armaduras.html')
+
+@app.route('/armas')
+def armas():
+    return render_template('armas.html')
+
+@app.route('/items')
+def items():
+    return render_template('items.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
